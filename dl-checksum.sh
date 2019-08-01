6f8ada87d06878827e0aa50c4cc0308406bdb306
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-08-01 13:02:24 -0500

    drop hadoop bundles. up to v1.8.1 and scala 2.12.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env sh
-VER='1.7.2'
+VER='1.8.1'
 DIR=~/Downloads
 MIRROR=https://www.apache.org/dist/flink/flink-${VER}
 
 dl()
 {
-    HADOOP_VER=$1
-    SCALA_VER=$2
-    URL=$MIRROR/flink-${VER}-bin-${HADOOP_VER}-scala_${SCALA_VER}.tgz.sha512
-    printf "${HADOOP_VER}:\n"
-    printf "# $URL\n"
-    printf "$SCALA_VER: sha512:"
-    curl -SsL $URL | awk '{print $1;}'
+    SCALA_VER=$1
+    URL=$MIRROR/flink-${VER}-bin-scala_${SCALA_VER}.tgz.sha512
+    printf "    # $URL\n"
+    printf "    '%s': sha512:%s\n" $SCALA_VER `curl -SsL $URL | awk '{print $1}'`
 }
 
-dl hadoop28 2.11
-dl hadoop27 2.11
-dl hadoop26 2.11
-dl hadoop24 2.11
+printf "  '%s':\n" $VER
+dl 2.11
+dl 2.12
