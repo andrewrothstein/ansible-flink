d380f3468772c0fb86a045a82fb4fb2dc3493342
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:31:30 -0500

    up to v1.9.2

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env sh
-VER='1.8.1'
 DIR=~/Downloads
-MIRROR=https://www.apache.org/dist/flink/flink-${VER}
+MIRROR=https://www.apache.org/dist/flink
 
 dl()
 {
-    SCALA_VER=$1
-    URL=$MIRROR/flink-${VER}-bin-scala_${SCALA_VER}.tgz.sha512
-    printf "    # $URL\n"
-    printf "    '%s': sha512:%s\n" $SCALA_VER `curl -SsL $URL | awk '{print $1}'`
+    local ver=$1
+    local scala_ver=$2
+    local url=$MIRROR//flink-${ver}/flink-${ver}-bin-scala_${scala_ver}.tgz.sha512
+    printf "    # %s\n" $url
+    printf "    '%s': sha512:%s\n" $scala_ver $(curl -SsL $url | awk '{print $1}')
 }
 
-printf "  '%s':\n" $VER
-dl 2.11
-dl 2.12
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver 2.11
+    dl $ver 2.12
+}
+
+dl_ver ${1:-1.9.2}
